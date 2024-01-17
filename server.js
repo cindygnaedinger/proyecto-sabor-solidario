41dfc8a3e17f9c3acc09ae1ed8e5746020a09d11
Victoria Torres <torresvictoriaf@outlook.com>
2024-01-17 18:49:37 -0300

    agregar cors

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import routerComedores from './routers/comedores.js';
-import routerDonadores from './routers/donadores.js';
-import DB from './models/saborSolidario-mongodb.js';
-import config from './config.js';
-
+import express from "express";
+import routerComedores from "./routers/comedores.js";
+import routerDonadores from "./routers/donadores.js";
+import DB from "./models/saborSolidario-mongodb.js";
+import config from "./config.js";
+import cors from "cors";
 
 // await DB.connectDB();
 DB.connectDB();
@@ -12,13 +12,20 @@ const PORT = config.PORT;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public', { extensions: ['html', 'htm', 'js'] }));
+app.use(express.static("public", { extensions: ["html", "htm", "js"] }));
 
-app.use('/api/comedores', routerComedores);
-app.use('/api/donadores', routerDonadores)
+app.use("/api/comedores", routerComedores);
+app.use("/api/donadores", routerDonadores);
 
-const server = app.listen(PORT, () => console.log(`Servidor Express escuchando en el puerto ${PORT}`));
-server.on('error', error => console.log(`Se produjo un error al intentar iniciar el servidor Express: ${error.message}`));
+const server = app.listen(PORT, () =>
+  console.log(`Servidor Express escuchando en el puerto ${PORT}`)
+);
+server.on("error", (error) =>
+  console.log(
+    `Se produjo un error al intentar iniciar el servidor Express: ${error.message}`
+  )
+);
