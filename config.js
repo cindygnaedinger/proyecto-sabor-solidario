e3be38777aad2f47710d21b08a3ff1d9be7803fb
Victoria Torres <torresvictoriaf@outlook.com>
2024-01-21 16:48:41 -0300

    cambiar base url config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
   MONGODB_CONNECTION_TIMEOUT: 200,
-  BASE_URL: "https://prueba-backend.netlify.app/.netlify/functions/server", // Modificar
+  BASE_URL: "https://sabor-solidario.netlify.app/.netlify/functions/server",
 };
 
 export default config;
